14bd17f573f3739c6c981c616f6351315d7375d2
kristina_lengvinayte <klengvinayte@gmail.com>
2023-01-01 16:51:27 +0200

    subscriptions & comments initial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_can_edit?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 
-  def current_user_can_edit?(event)
-    user_signed_in? && event.user == current_user
+  def current_user_can_edit?(model)
+    # Если у модели есть юзер и он залогиненный, пробуем у неё взять .event
+    # Если он есть, проверяем его юзера на равенство current_user.
+    user_signed_in? && (
+      model.user == current_user ||
+        (model.try(:event).present? && model.event.user == current_user)
+    )
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,13 @@
 class Event < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
+  has_many :subscribers, through: :subscriptions, source: :user
+
   belongs_to :user
 
   validates :user, presence: true
 
-  validates :title, presence: true, length: {maximum: 255}
+  validates :title, presence: true, length: { maximum: 255 }
   validates :address, presence: true
   validates :datetime, presence: true
 end
